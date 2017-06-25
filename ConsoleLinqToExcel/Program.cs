09c282d4bd3505c79c90ed41da83ef3341e2154a
leo-9 <tec.lrodriguez@gmail.com>
2017-06-25 20:16:32 -0300

    segundo commit

---
+++ ConsoleLinqToExcel/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EntititesExcel;
+using BusinessExcel;
+
+namespace ConsoleLinqToExcel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<PersonaExcel> listaPersonas = new List<PersonaExcel>();
+
+            listaPersonas = CargarLista();
+            foreach (var p in listaPersonas)
+            {
+                Console.WriteLine("Nombre: " + p.nombreCompleto + ", " + p.apellido);
+            }
+
+            Console.ReadKey();
+        }
+
+        public static List<PersonaExcel> CargarLista()
+        {
+            try
+            {
+                ExcelBusiness excelBusiness = new ExcelBusiness("C:\\Leo\\Temp\\BASE DE DATOS - CUMPLEAÑOS (ACTUAL)Import.xlsx");
+                List<PersonaExcel> lista = (List<PersonaExcel>)excelBusiness.RetornarRowExcel("BASE TOTAL DE CLIENTES CUMPLE");
+
+                foreach (PersonaExcel persona in lista)
+                {
+                    string nombre = persona.nombreCompleto;
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ Repositories/ExcelRepository.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EntititesExcel;
+using LinqToExcel;
+
+namespace RepositoriesExcel
+{
+    public class ExcelRepository
+    {
+        public string pathArchivo { get; set; }
+
+        public ExcelRepository(string _pathArchivo)
+        {
+            pathArchivo = _pathArchivo;
+        }
+
+        public object CargarArchivo(string _hoja)
+        {
+            try
+            {
+                var excel = new ExcelQueryFactory(pathArchivo);
+
+                var personas = from p in excel.Worksheet<PersonaExcel>(_hoja)
+                               select p;
+
+                return personas.ToList();
+                //return personas;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+        public object CargarArchivoFuncionaOK1()
+        {
+            try
+            {
+                var excel = new ExcelQueryFactory(pathArchivo);
+
+                var personas = from p in excel.Worksheet<PersonaExcel>("BASE TOTAL DE CLIENTES CUMPLE")
+                               select p;
+
+                return personas.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public object CargarArchivoFuncionaOK()
+        {
+            try
+            {
+                var excel = new ExcelQueryFactory(pathArchivo);
+
+                var personas = from p in excel.Worksheet("Hoja1")
+                               select new PersonaExcel { nombreCompleto = p["nombreCompleto"] };
+
+                return personas.ToList();
+            }
+            catch (Exception ex)
+            {                
+                throw ex;
+            }
+        }
+    }
+}
